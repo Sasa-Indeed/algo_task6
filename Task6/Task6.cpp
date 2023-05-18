268bded2e61297fe4fa7e71a909171cc0767d344
Zaituny <mohamedinv.580@gmail.com>
2023-05-18 18:00:07 +0300

    created second solution

--- Task6/Task6.cpp
+++ Task6/Task6.cpp
@@ -8,7 +8,6 @@ enum genuinity  { Genuine, CounterfeitLight, CounterfeitHeavy};
 
 
 typedef struct {
-    int id;
     double weight;
     genuinity gen;
 }Coin;
@@ -22,7 +21,7 @@ typedef struct {
 
 1  : left pan is up
 */
-int scale(Coin* rightPan, Coin* leftPan,int numCoins) {
+int scale(Coin* leftPan, Coin* rightPan,int numCoins) {
     double rweight = 0, lweight = 0;
 
     for (int i = 0; i < numCoins; i++){
@@ -91,18 +90,15 @@ Coin* findFakeInGroupOfThree(Coin* groupOfCoins){
 }
 
 /*return false if all coins are geniune*/
-bool fakeCoin(Coin* coins,  int numCoins,Coin* fake) {
+bool fakeCoin(Coin* coins,  int numCoins) {
     //for dividing the array of coins into smaller arrays
-    if (numCoins == 1 || numCoins == 2) {
-        std::cout << "Impossible to determine whether there is a fake coin or not using a balance scale"<<std::endl;
-    }
-    else if (numCoins == 3) {
+    if (numCoins == 3) {
         Coin* fake = findFakeInGroupOfThree(coins);
         if (fake == NULL) {
-            std::cout << "All coins are genuine" << std::endl;
+            return false;
         }
         else {
-            std::cout << "Coin number: " << fake->id << " is fake" << std::endl;
+            return true;
 
         }
     }
@@ -110,30 +106,81 @@ bool fakeCoin(Coin* coins,  int numCoins,Coin* fake) {
         int numofGroups = (numCoins % 3 == 0)? numCoins/3 : numCoins/3 + 1;
         int n = 0;
 
-        Coin** group = new Coin* [numofGroups];
-
-        for (int i = 0; i < n; i++) {
-            group[i] = new Coin[3];
-        }
-
-        for (int i = 0; i < numCoins; i++) {
-            group[i / 3][i % 3] = coins[i];
-        }
-        Coin** result = new Coin*[numofGroups];
+        Coin*** group = new Coin** [numofGroups];
 
         for (int i = 0; i < numofGroups; i++) {
-            if (i == numofGroups - 1) {
-                // implement logic to check if last group has less than 3 elements
-                // and act acordingly
+            group[i] = new Coin*[3] {};
+        }
+        for (int i = 0; i < numCoins; i++) {
+            group[i / 3][i % 3] = &coins[i];
+        }
+
+        for (int i = 0; i < numofGroups; i++) {
+            if (i == numofGroups - 1 && numCoins % 3 == 1) {
+                if (scale(group[i - 1][0], group[i][0], 1) == 0) {
+                    group[i][0]->gen = Genuine;
+                    return false;
+                }
+                else if (scale(group[i - 1][0], group[i][0], 1) == -1) {
+                    group[i][0]->gen = CounterfeitLight;
+                    return true;
+                }
+                else {
+                    group[i][0]->gen = CounterfeitHeavy;
+                    return true;
+                }
             }
-            result[i] = findFakeInGroupOfThree(group[i]);
+            else if (i == numofGroups - 1 && numCoins % 3 == 2) {
+                group[i][2] = group[i - 1][0];
+                Coin* fake = findFakeInGroupOfThree(*group[i]);
+                if (fake == NULL) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+            Coin* fake = findFakeInGroupOfThree(*group[i]);
+            if (fake != NULL) {
+                return true;
+            }
         }
     }
 }
 
 int main(){
-    int  a [5];
-
-    std::cout << "Hello World!\n";
+    Coin coin1 = {1.0};
+    Coin coin2 = { 1.0 };
+    Coin coin3 = { 1.0 };
+    Coin coin4 = { 1.0 };
+    Coin coin5 = { 1.0 };
+    Coin coin6 = { 1.0 };
+    Coin coin7 = { 1.0 };
+    Coin coin8 = { 1.0 };
+    Coin coin9 = { 1.0 };
+    Coin coin10 = { 1.0 };
+    Coin coin11 = { 1.0 };
+    Coin coin12 = { 1.1 };
+    Coin test[]{coin1,
+        coin2,
+        coin3,
+        coin4,
+        coin5,
+        coin6,
+        coin7,
+        coin8,
+        coin9,
+        coin10,
+        coin11,
+        coin12};
+    if (sizeof(test) / sizeof(Coin) < 3) {
+        std::cout << "invalid number of coins"<<std::endl;
+    }
+    else {
+        bool result = fakeCoin(test, 12);
+        for (int i = 0; i < 12; i++) {
+            std::cout << "Coin " << i << " : " << test[i].gen << std::endl;
+        }
+    }
 }
 
