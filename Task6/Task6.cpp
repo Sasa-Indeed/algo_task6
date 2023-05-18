b16519829df61fd7a3b0aff19a136a0674f2027b
Sasa-Indeed <ahmed382002@gmail.com>
2023-05-18 08:21:43 +0100

    Progress is progress

--- Task6/Task6.cpp
+++ Task6/Task6.cpp
@@ -39,20 +39,53 @@ int scale(Coin* rightPan, Coin* leftPan,int numCoins) {
 
 }
 
+
+//NULL ----> all coins are genuine
 Coin* findFakeInGroupOfThree(Coin* groupOfCoins){
-    if (scale(&groupOfCoins[0], &groupOfCoins[1], 1) == 0) {
-        if (scale(&groupOfCoins[1], &groupOfCoins[2], 1) == 0) {
+    //1:2
+    int scaleResult = scale(&groupOfCoins[0], &groupOfCoins[1], 1);
+
+    if (scaleResult == 0) {
+        groupOfCoins[0].gen = Genuine;
+        groupOfCoins[1].gen = Genuine;
+        scaleResult = scale(&groupOfCoins[1], &groupOfCoins[2], 1); // 2 : 3
+        // 1 = 2
+        if (scaleResult == 0) {
+            //All coins are the same weight
+            groupOfCoins[2].gen = Genuine;
             return NULL;
-        }
-        else
+        }else {
+            /*Here the third coin is either heavier or lighter than the first two*/
+            (scaleResult > 0) ? groupOfCoins[2].gen = CounterfeitHeavy : groupOfCoins[2].gen = CounterfeitLight;
             return &groupOfCoins[2];
-    }
-    else {
+        }
+    }else if(scaleResult == -1) {
+        /*1 : 2 and left pan went down*/
         if (scale(&groupOfCoins[0], &groupOfCoins[2], 1) == 0) {
+            groupOfCoins[1].gen = CounterfeitLight;
+            groupOfCoins[0].gen = Genuine;
+            groupOfCoins[2].gen = Genuine;
             return &groupOfCoins[1];
         }
-        else
+        else {
+            groupOfCoins[1].gen = Genuine;
+            groupOfCoins[2].gen = Genuine;
+            groupOfCoins[0].gen = CounterfeitHeavy;
             return &groupOfCoins[0];
+        }
+    }else{
+        if (scale(&groupOfCoins[0], &groupOfCoins[2], 1) == 0) {
+            groupOfCoins[1].gen = CounterfeitHeavy;
+            groupOfCoins[2].gen = Genuine;
+            groupOfCoins[0].gen = Genuine;
+            return &groupOfCoins[1];
+        }
+        else {
+            groupOfCoins[2].gen = Genuine;
+            groupOfCoins[1].gen = Genuine;
+            groupOfCoins[0].gen = CounterfeitLight;
+            return &groupOfCoins[0];
+        }
     }
 }
 
